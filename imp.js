76dc8764fbce5d0ad72afe75a8e0eb23b78c5140
Aryan Mishra <carrer1270@gmail.com>
2026-05-24 22:59:17 +0530

    adding notes related to custom components

---
+++ imp.js
@@ -0,0 +1,15 @@
+// WHY REACT??
+
+// 1. ITS COMPOSABLE => WE HAVE THE TOOLS AVAILABLE TO CREATE REUSABLE AND INTERCHANGABLE PIECES OF THE WEB THAT CAN BE COMBINED IN A LOT OF DIFFRENT WAYS TO CREATE COMPLEX SYSTEM 
+
+// EX => BOOTSTRAP LIBRARY SEE IN IMAGE NAV BAR SHOW 
+
+// NOW IF YOU WANT TO CHANGE THIS HTML FILE YOU AHVE TO CHANGE AT 100 PLACE BUT WITH REACT YOU CAN CHANGE IT ONLY TWO OR 3 PLACES USING CUSTOM COMPONENTS[componts that are recomoposable] => these are created not native to HTML elemnts 
+
+/*
+<body>
+<MyAwesomeNavbar/>   // these are more composable i've given these components silly name so to identify its custom there are things it reprents that bootstrap code this my awesome navbar ensacuplate all of the code and then anytime in my doc i want to include a navbar i can simply renddera copy of awesome navbar and i will get everything that comes with the code that we see here and if i want tp make a change i can make that chage in one place and it will reflect everwhere now my awesome  navbar is used 
+<MainContent/>
+<MyAwesomeFooter/>
+</body>
+*/
