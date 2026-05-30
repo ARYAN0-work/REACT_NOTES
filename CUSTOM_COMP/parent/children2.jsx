71ca74fbce0175cc51346fdeab9eaec267263506
Aryan Mishra <carrer1270@gmail.com>
2026-05-30 20:23:06 +0530

    adding notes related parent and children elemenets

---
+++ CUSTOM_COMP/parent/children2.jsx
@@ -0,0 +1,7 @@
+/** Challenge:
+ * Move the `main` element into its own component called "MainContent"
+ * and render that component inside the Page component.
+ *
+ * Do the same with the `footer` element, moving it into a new
+ * component called "Footer"
+ */
