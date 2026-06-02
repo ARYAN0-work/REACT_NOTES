6e2df4a74342c4b975a3401b23ac5d82952fd6ef
Aryan Mishra <carrer1270@gmail.com>
2026-06-03 03:00:56 +0530

    adding components

---
+++ PROJECT_SETUP/components/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar(){
+    return(
+        <header>
+            <nav>
+                <img src="/images/react-logo.png" alt="react-logo"></img>// yha bhi ../
+                <span>React Facts</span>
+            </nav>
+        </header>
+    )
+}
