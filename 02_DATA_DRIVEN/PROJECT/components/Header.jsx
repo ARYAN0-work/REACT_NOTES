d4aad19aa91caff8ea3b6a390160700d99011bff
Aryan Mishra <carrer1270@gmail.com>
2026-06-05 20:18:40 +0530

    adding components

---
+++ 02_DATA_DRIVEN/PROJECT/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <Header>
+            <img src="globe.png " alt="globe.iconc"/>// if classname globe then in css access will header img.globe 
+            <h1>my teavel journal</h1>
+        </Header>
+    )
+}
