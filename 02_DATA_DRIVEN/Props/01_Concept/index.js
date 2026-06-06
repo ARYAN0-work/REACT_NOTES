e80da7b506d9e0c46135cf4b620c24fa0b2e44dc
Aryan Mishra <carrer1270@gmail.com>
2026-06-06 20:52:12 +0530

    adding overview of props

---
+++ 02_DATA_DRIVEN/Props/01_Concept/index.js
@@ -0,0 +1,9 @@
+function addTwoTogether() {
+    return 1+2
+}
+
+// its synatxially correct but it will always give 3 : limited in capabilties but if i did this it can be useful
+
+function addTwoTogether(a,b) {
+    return a+b
+}
