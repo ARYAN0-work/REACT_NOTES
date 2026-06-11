fe656548df15be2275aa7d1fe5192c7696ce4f84
Aryan Mishra <carrer1270@gmail.com>
2026-06-11 20:00:26 +0530

    Key Prop

--- 02_DATA_DRIVEN/PROJECT/App.jsx
+++ 02_DATA_DRIVEN/PROJECT/App.jsx
@@ -1,17 +1,28 @@
 import Entry from "./components/Entry"
 import Header from "./components/Header"
 import data from "./data";
+
+//we are gonna faac ethis error a lot in react while wroking with array  when we are gonna take an array of data and turn into array of components or elements
+
+// react have intresting system under the hood that it uses to keep track of what order verything is in when you give it an array of react elements ,functionalty => in order to add and remove react keeps a track of react needs this uniques key prop default its key  
 export default function App(){
 
         const entryElements =data.map((entry)=>{
             return(
                 <Entry
-                   //img={{
-                   // src:entry.img.src,
-                   // alt:entry.img.alt we can short this bit 
+                // when we get data from database is the data or array of data or that array of data will usually have an ID attribute associated with it and this is actually managed by DB ensure that id unique 
+                 // key={1}
+                 key={entry.id}// warning gone =>use this often
 
-                  //}}
-                  img={entry.img}// entry component expexting a image prop yo be an object and that object needs to have an source property or an SRC ,alt property and in our data.js img is shaped in that way => forshadowing
+                 //if array doesnt have an id -> these what people do dont doi this 
+
+/**
+ * data.map((entry,index)=>{
+ * return(
+ *       <Entry
+ *            key={index}
+ */
+                  img={entry.img}
                    title={entry.title}
                    country={entry.country}
                    googleMapsLink={entry.googleMapsLink}
@@ -28,4 +39,4 @@ export default function App(){
     )
 }
 
-// and now our project is complete but 
+// now the advantage we have the display is  a fn of data we're pulling into our app this just means that if we wereto come to our data js file and lets say just copy last three items in data js then check in app you go that repeated items by just manipulating data we have xtra components in future we are gonna pull this data in from API but thats beside point at this time 
