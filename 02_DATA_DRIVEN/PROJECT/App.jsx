7798f47289c77ce3b7e521208751202efa2a70de
Aryan Mishra <carrer1270@gmail.com>
2026-06-12 19:46:19 +0530

    adding all the reming dinsihes

--- 02_DATA_DRIVEN/PROJECT/App.jsx
+++ 02_DATA_DRIVEN/PROJECT/App.jsx
@@ -6,18 +6,10 @@ export default function App(){
 
         const entryElements =data.map((entry)=>{
             return(
-                // if  theres gonna be a lot of data then do this to save time we are no longer reciving img prop fro just bypassing the error you can de this entry.jsx
-                // src ={props.img?.src} 
-                // do console.log you will get the whole object => obbserve we are reciving an entry prop so do this
-                /**
-                  src={props.entry.img.src}
-                   alt={props.entry.img.alt}
-                   */ 
+               
                 <Entry
                  key={entry.id}
-                 //object={entry}
-                  enrty ={entry}
-                  // now everythings broken => see console 
+                 {...entry}
                 />
             )
         })
---
+++ 02_DATA_DRIVEN/overview.md
@@ -0,0 +1,7 @@
+
+### Project Outcome
+
+Built a **Travel Journal** application that:
+- Uses reusable React components.
+- Passes data through props.
+- Generates UI from an array of travel data using `.map()`.
