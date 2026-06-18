f43b64b8fdc200a92beed4d1fe28145f9b2891a1
Aryan Mishra <carrer1270@gmail.com>
2026-06-19 01:25:37 +0530

    adding basic setup for our new project

--- SECTION_1/SETUP/setup.md
+++
@@ -1,5 +0,0 @@
-# gonna use vite(tool)
-
-- first  download nvm
-- npm create vite@latest
-- then choose and npm run dev
--- SECTION_1/organizing_componetns/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return(
-        <footer>
-            <small>© 2026 web development. All rights reserved</small>
-            </footer>
-    )
-}
