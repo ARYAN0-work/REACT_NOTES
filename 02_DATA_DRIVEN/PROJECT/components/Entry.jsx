0e95222f75564f2170933a4ac4a87e94b7f0858b
Aryan Mishra <carrer1270@gmail.com>
2026-06-20 21:34:15 +0530

    fixing bugs

--- 02_DATA_DRIVEN/PROJECT/components/Entry.jsx
+++ 02_DATA_DRIVEN/PROJECT/components/Entry.jsx
@@ -1,20 +1,26 @@
-export default function Entry() {
-    return(
-         // so here is all surrounding dev or html article element- i can put a flexbox and seprate two elements images & data
+export default function Entry(props) {
+    return (
         <article className="journal-entry">
             <div className="main-image-container">
-                 <img className="main-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+                <img 
+                    className="main-image"
+                    src={props.img.src}
+                    alt={props.img.alt}
+                />
             </div>
-            // now i can surround these 3 eleemnet in div and later flex box
-            <div>
-                <img src="../images/marker.png" alt="map marker icon"/>
-                <span>Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
-                <h2>Mount Fuji</h2>
-                <p>12 Jan, 2021 - 24 Jan, 2021</p>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <div className="info-container">
+                <img 
+                    className="marker"
+                    src="../images/marker.png" 
+                    alt="map marker icon"
+                />
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink} target="_blank">View on Google Maps</a>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.dates}</p>
+                <p className="entry-text">{props.text}</p>
             </div>
+            
         </article>
-        
     )
 }
--- 02_DATA_DRIVEN/PROJECT/components/Header.jsx
+++ 02_DATA_DRIVEN/PROJECT/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <Header>
-            <img src="globe.png " alt="globe.iconc"/>// if classname globe then in css access will header img.globe 
+            <img src="globe.png " alt="globe.iconc"/> 
             <h1>my teavel journal</h1>
         </Header>
     )
