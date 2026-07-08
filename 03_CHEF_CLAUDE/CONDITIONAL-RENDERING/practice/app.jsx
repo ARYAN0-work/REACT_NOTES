91d28b6874f09ae73577d3c71f3a19b14ac764f7
Aryan Mishra <carrer1270@gmail.com>
2026-07-08 22:11:46 +0530

    adding practice notes of condition redening

---
+++ 03_CHEF_CLAUDE/CONDITIONAL-RENDERING/practice/app.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+export default function App() {
+    const [messages, setMessages] = React.useState(["a", "b"])
+    let text
+
+    /**
+     * Challenge:
+     * - If there are no unread messages, display "You're all caught up!"
+     * - If there's exactly 1 unread message, it should read "You have
+     *   1 unread message (singular)"
+     * - If there are > 1 unread messages, display "You have <n> unread
+     *   messages" (plural)
+     */
+
+    if (messages.length===0) {
+        text = "You're all caught up"
+    } else if(messages.length === 1){
+        text = "You have 1 unread messages"
+    } else{
+        text = `You have ${messages.length} unread messages`
+    }
+    
+    return (
+        <div>
+            <h1>{text}</h1>
+        </div>
+    )
+}
